21e8a322c10239e9012bef91b19fdfa067361aec
pradipj <pradipj@dhitech.solutions>
2025-01-14 15:35:26 +0530

    added code for the formating

---
+++ src/pages/Post.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+    </div>
+  );
+}
+
+export default Post;
---
+++ src/pages/Upload.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Upload() {
+  return (
+    <div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+      <div>
+        Welcome to the Upload Page abc dfa trag aer e r ad loram 100 fdadf er e
+        re r
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
